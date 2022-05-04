fcd386375f29b6bc6be66168e55212b1e07894d5
SamSotsky <sam.sotsky01@gmail.com>
2022-05-04 12:48:04 -0700

    JSX

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello Dojo! </h1> 
+      <p>Things I need to do:</p>
+      <li>Learn React</li>
+      <li>Walk dog</li>
+      <li>Pet dog</li>
+      <li>Eat dog</li>
     </div>
   );
 }
